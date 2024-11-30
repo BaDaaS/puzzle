e465a1065398176c2dd6d94214e94fb3e11a2bf3
Danny Willems <be.danny.willems@gmail.com>
2024-11-30 11:07:26 +0100

    Puzzle/urls: remove unused imported include

--- puzzle/urls.py
+++ puzzle/urls.py
@@ -3,7 +3,7 @@ URL configuration
 """
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
