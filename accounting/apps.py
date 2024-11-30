32adf32bfceb3529d0babaf0eab9ba61d2c37521
Danny Willems <be.danny.willems@gmail.com>
2024-11-30 14:17:34 +0100

    Load cache when apps ready

--- accounting/apps.py
+++ accounting/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class BaseConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "accounting"
+
+    def ready(self):
+        from accounting.cache import load_cache
+
+        load_cache()
