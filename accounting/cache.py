a6cf28ab5dff7ee73851a0e1a88b294809d964bb
Danny Willems <be.danny.willems@gmail.com>
2024-12-01 16:05:18 +0100

    CI/cache: only do when the database is ready

--- accounting/cache.py
+++ accounting/cache.py
@@ -1,5 +1,6 @@
-from django.core.cache import cache
 from accounting.models import Entity
+from common.utils import is_database_synchronized
+from django.core.cache import cache
 import logging
 
 
@@ -7,5 +8,8 @@ LOGGER = logging.getLogger(__name__)
 
 
 def load_cache():
+    if is_database_synchronized("default"):
+        LOGGER.debug("Database is not synchronized. Skipping cache loading")
+        return
     cache.set("Entity", Entity.objects.all())
     LOGGER.debug("Entity cache set")
